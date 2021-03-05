6b569cc1ca40a46261c2c19dcbd252b434ebe4c3
Ezequiel mross <ezequielmross@gmail.com>
2021-03-05 11:33:10 -0300

    Add tailwind and fix performance

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  // purge: {
+  //   enabled: true,
+  //   content: ['./index.html'],
+  // },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
